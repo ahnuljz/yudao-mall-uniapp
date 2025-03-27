2d74a9e82558b880cb85ce0c18c53c3f3d33b7b9
shamor <yzban@mail.ahnu.edu.cn>
2025-03-27 22:04:06 +0800

    官方部分优化

--- App.vue
+++ App.vue
@@ -1,37 +1,39 @@
 <script setup>
-  import { onLaunch, onShow, onError } from '@dcloudio/uni-app';
-  import { ShoproInit } from './sheep';
+import { onLaunch, onShow, onError } from '@dcloudio/uni-app';
+import { ShoproInit } from './sheep';
 
-  onLaunch(() => {
-    // 延时隐藏原生导航栏
-    setTimeout(() => {
-      uni.hideTabBar();
-    }, 200);
-
-    // 加载Shopro底层依赖
-    ShoproInit();
-  });
-
-  onShow((options) => {
-    // #ifdef APP-PLUS
-    // 获取urlSchemes参数
-    const args = plus.runtime.arguments;
-    if (args) {
-    }
-
-    // 获取剪贴板
-    uni.getClipboardData({
-      success: (res) => {},
+onLaunch(() => {
+  // 延时隐藏原生导航栏
+  setTimeout(() => {
+    uni.hideTabBar({
+      fail: () => { },
     });
-    // #endif
+  }, 200);
 
-    // #ifdef MP-WEIXIN
-    // 确认收货回调结果
-    console.log(options, 'options');
-    // #endif
+  // 加载Shopro底层依赖
+  ShoproInit();
+});
+
+onShow((options) => {
+  // #ifdef APP-PLUS
+  // 获取urlSchemes参数
+  const args = plus.runtime.arguments;
+  if (args) {
+  }
+
+  // 获取剪贴板
+  uni.getClipboardData({
+    success: (res) => { },
   });
+  // #endif
+
+  // #ifdef MP-WEIXIN
+  // 确认收货回调结果
+  console.log(options, 'options');
+  // #endif
+});
 </script>
 
 <style lang="scss">
-  @import '@/sheep/scss/index.scss';
+@import '@/sheep/scss/index.scss';
 </style>
